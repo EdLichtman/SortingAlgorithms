77183089fbc48c0ffa7e6f19b6dbfe8ed8787a71
eddie <e.lichtman2@gmail.com>
2018-07-17 20:47:33 -0500

    Adds vscode profile settings to gitignore. Adds initial solution. Adds first failing test.

---
+++ SortingAlgorithms/SortingAlgorithmsUnitTests/HeapSortTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace SortingAlgorithmsUnitTests
+{
+    [TestFixture]
+    public class HeapSortTests
+    {
+        [Test]
+        public void HeapSort_sorts_data_set_when_called()
+        {
+            var dataset = new[] {1, 6, 3, 2, 8, 5, 9};
+            dataset = dataset.OrderBy(dataitem => dataitem).ToArray();
+            var systemSortedDataSet = dataset.OrderBy(dataitem => dataitem).ToList();
+
+            Assert.That(dataset, Is.EqualTo(systemSortedDataSet));
+        }
+    }
+}
